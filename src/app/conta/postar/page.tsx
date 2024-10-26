6649bf2f2b3073cb9c880fd649ed07116e228777
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-10-26 17:10:26 -0300

    add: new routes: conta, foto, login, perfil

---
+++ src/app/conta/postar/page.tsx
@@ -0,0 +1,7 @@
+export default async function PostarPage() {
+  return (
+    <main>
+      <h1>postar</h1>
+    </main>
+  );
+}
---
+++ src/app/login/criar/page.tsx
@@ -0,0 +1,7 @@
+export default async function CriarPage() {
+  return (
+    <main>
+      <h1>Criar Conta</h1>
+    </main>
+  );
+}
