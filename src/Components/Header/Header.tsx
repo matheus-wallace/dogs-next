5a03ae93c31ab82ad4f474f22cfb1d78063b0f58
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-10-26 17:28:29 -0300

    add: add header

---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './header.module.css';
+import Image from 'next/image';
+
+const Header = () => {
+  const user = true;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} href={'/'}>
+          <Image src={'/Assets/dogs.svg'} alt="Dogs" width={28} height={22} priority />
+        </Link>
+        {user ? (
+          <Link className={styles.login} href={'/conta'}>
+            Dogs
+          </Link>
+        ) : (
+          <Link className={styles.login} href={'/login'}>
+            Login/Criar
+          </Link>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
