0a091c67481fb0081647908d28f8f9a0723c7fcc
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-10-30 23:33:18 -0300

    add: new user context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,25 +3,31 @@ import './globals.css';
 import { type_second } from '@/functions/fonts';
 import Header from '@/Components/Header/Header';
 import Footer from '@/Components/Footer/Footer';
+import { UserContextProvider } from '@/context/userContext';
+import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
   description: 'Rede social para cachorros',
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const { data: user } = await userGet();
+
   return (
     <html lang="pt-br">
       <body className={type_second.variable}>
-        <div className="App">
-          <Header />
-          <main className="appBody">{children}</main>
-          <Footer />
-        </div>
+        <UserContextProvider user={user}>
+          <div className="App">
+            <Header />
+            <main className="appBody">{children}</main>
+            <Footer />
+          </div>
+        </UserContextProvider>
       </body>
     </html>
   );
