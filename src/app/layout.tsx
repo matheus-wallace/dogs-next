d6b4ac77a34b8a73ca9248b4676e4666d3b66faa
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-11-30 23:28:40 -0300

    feat: adicionado modal ao abrir o foto

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,23 +8,26 @@ import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
-  description: 'Rede social para cachorros',
+  description: 'Rede social para cachorros.',
 };
 
 export default async function RootLayout({
   children,
+  modal,
 }: Readonly<{
   children: React.ReactNode;
+  modal: React.ReactNode;
 }>) {
   const { data: user } = await userGet();
 
   return (
-    <html lang="pt-br">
+    <html lang="pt-BR">
       <body className={type_second.variable}>
         <UserContextProvider user={user}>
           <div className="App">
             <Header />
-            <main className="appBody">{children}</main>
+            <main className="AppBody">{children}</main>
+            <div>{modal}</div>
             <Footer />
           </div>
         </UserContextProvider>
