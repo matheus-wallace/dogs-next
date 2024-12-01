e9e32b23db313f1a904cedff5787c3ce4ffa8545
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-11-30 21:46:39 -0300

    feat: add new token validation

--- src/context/userContext.tsx
+++ src/context/userContext.tsx
@@ -1,10 +1,8 @@
 'use client';
-import React, { createContext, useContext, useState } from 'react';
 
-type IUserContext = {
-  user: User | null;
-  setUser: React.Dispatch<React.SetStateAction<User | null>>;
-};
+import logout from '@/actions/logout';
+import validateToken from '@/actions/validate-token';
+import React from 'react';
 
 type User = {
   id: number;
@@ -13,16 +11,31 @@ type User = {
   email: string;
 };
 
-const UserContext = createContext<IUserContext | null>(null);
+type IUserContext = {
+  user: User | null;
+  setUser: React.Dispatch<React.SetStateAction<User | null>>;
+};
+
+const UserContext = React.createContext<IUserContext | null>(null);
 
 export const useUser = () => {
   const context = React.useContext(UserContext);
-  if (context === null) throw new Error('useContext deve estar dento do provider');
+  if (context === null) {
+    throw new Error('useContext deve estar dentro do Provider');
+  }
   return context;
 };
 
 export function UserContextProvider({ children, user }: { children: React.ReactNode; user: User | null }) {
-  const [userState, setUser] = useState<User | null>(user);
+  const [userState, setUser] = React.useState<User | null>(user);
+
+  React.useEffect(() => {
+    async function validate() {
+      const { ok } = await validateToken();
+      if (!ok) await logout();
+    }
+    if (userState) validate();
+  }, [userState]);
 
   return <UserContext.Provider value={{ user: userState, setUser }}>{children}</UserContext.Provider>;
 }
