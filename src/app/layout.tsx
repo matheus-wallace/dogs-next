917fafc1cebaf4d379aa31d88d9c01aab375f89c
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-11-30 23:52:30 -0300

    fix: ajustado bug com `Footer` ao adicionar a modal

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import userGet from '@/actions/user-get';
 
 export const metadata: Metadata = {
   title: 'Dogs Next',
-  description: 'Rede social para cachorros.',
+  description: 'Rede social para cachorros',
 };
 
 export default async function RootLayout({
@@ -21,12 +21,12 @@ export default async function RootLayout({
   const { data: user } = await userGet();
 
   return (
-    <html lang="pt-BR">
+    <html lang="pt-br">
       <body className={type_second.variable}>
         <UserContextProvider user={user}>
           <div className="App">
             <Header />
-            <main className="AppBody">{children}</main>
+            <main className="appBody">{children}</main>
             <div>{modal}</div>
             <Footer />
           </div>
